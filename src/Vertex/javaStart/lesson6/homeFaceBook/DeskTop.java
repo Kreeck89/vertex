58e71b22924b418f99eecf43d11beb7294f09c7d
Kreeck <Kreeck@mail.ua>
2017-10-29 01:08:45 +0300

    Home Task. Analog FaceBook. in process.

--- src/Vertex/javaStart/lesson6/homeFaceBook/DeskTop.java
+++ src/Vertex/javaStart/lesson6/homeFaceBook/DeskTop.java
@@ -1,12 +1,13 @@
 package Vertex.javaStart.lesson6.homeFaceBook;
 
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class DeskTop extends Music {
 
-    public void deskPrint() {
-        FileWriter writer = new FileWriter();
+    public void deskPrint() throws IOException {
+        FileWriter writer = new FileWriter("");
         Scanner scanner = new Scanner(System.in);
         System.out.println("It is your board:");
         String usersText = scanner.nextLine();
